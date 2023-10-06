510119c4fae71f04337c49e817f8f74e8497e9ef
Jonathan Dotore <jonathandotore@outlook.com>
2023-10-05 22:21:11 -0300

    Adding functionalities to my .js file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,26 @@
-//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
-const buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-//5. At the top of the game.js file, create a new empty array called gamePattern.
-const gamePattern = [];
+var gamePattern = [];
+var userClickedPattern = [];
 
-//1. Inside game.js create a new function called nextSequence()
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+
+});
+
 function nextSequence() {
-
-    //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
-    let randomNumber = Math.floor(Math.random() * 4);
-
-    //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
-    let randomChosenColour = buttonColours[randomNumber];
-
-    //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("./assets/sounds/" + name + ".mp3");
+    audio.play();
 }
